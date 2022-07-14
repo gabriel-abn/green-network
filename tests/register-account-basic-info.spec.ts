99cf0588a6890319293593dfa9a9e2848f26341c
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2022-07-14 12:05:06 -0300

    ensure user's cpf is valid comparing its lenght

--- tests/register-account-basic-info.spec.ts
+++ tests/register-account-basic-info.spec.ts
@@ -12,16 +12,38 @@ type RegisterAccountBasicInfoResponseDTO = {
   message: string;
 };
 
+interface ICPFCheckerService {
+  check(cpf: string): boolean;
+}
+
+class CPFCheckerSpy implements ICPFCheckerService {
+  check(cpf: string): boolean {
+    if (cpf.length != 11) {
+      return false;
+    }
+    return true;
+  }
+}
+
 class RegisterAccountBasicInfoUseCase {
+  constructor(private cpfChecker: ICPFCheckerService) {}
   execute(
     params: RegisterAccountBasicInfoRequestDTO
   ): RegisterAccountBasicInfoResponseDTO {
+    if (!this.cpfChecker.check(params.cpf)) {
+      return {
+        message: "Invalid CPF",
+        status: 400,
+      };
+    }
+
     if (new Date().getFullYear() - params.birthDate.getFullYear() < 21) {
       return {
         message: "Age not allowed",
         status: 400,
       };
     }
+
     return {
       data: {
         ...params,
@@ -32,9 +54,16 @@ class RegisterAccountBasicInfoUseCase {
   }
 }
 
+const makeSut = () => {
+  const cpfChecker = new CPFCheckerSpy();
+  const sut = new RegisterAccountBasicInfoUseCase(cpfChecker);
+
+  return { sut };
+};
+
 describe("First step of user register", () => {
   it("should receive correct params and return account info and a status 400", () => {
-    const sut = new RegisterAccountBasicInfoUseCase();
+    const { sut } = makeSut();
     const request = {
       name: "Gabriel Antonio",
       birthDate: new Date("2000-09-20"),
@@ -49,7 +78,7 @@ describe("First step of user register", () => {
     expect(httpResponse.status).toBe(200);
   });
   it("should not allow users registration with less than 18 years old", () => {
-    const sut = new RegisterAccountBasicInfoUseCase();
+    const { sut } = makeSut();
     const request = {
       name: "Gabriel Antonio",
       birthDate: new Date("2004-09-20"),
@@ -59,5 +88,17 @@ describe("First step of user register", () => {
     };
     const httpResponse = sut.execute({ ...request });
     expect(httpResponse.status).toBe(400);
+  });
+  it("should ensure user's cpf is correct", async () => {
+    const { sut } = makeSut();
+    const request = {
+      name: "Gabriel Antonio",
+      birthDate: new Date("2000-09-20"),
+      address: "Rua Francisco Teles, 41, Alvorada",
+      cpf: "123456789",
+      rg: "12345678",
+    };
+    const httpResponse = sut.execute({ ...request });
+    expect(httpResponse.message).toBe("Invalid CPF");
   });
 });
